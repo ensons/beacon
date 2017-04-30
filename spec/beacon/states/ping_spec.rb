58e92fec7306375c0be21c65941f4ec8748774a4
ensons <info@ensons.de>
2017-04-30 13:39:24 +0200

    Use webmock to test external calls

--- spec/beacon/states/ping_spec.rb
+++ spec/beacon/states/ping_spec.rb
@@ -2,18 +2,29 @@ require 'spec_helper'
 
 RSpec.describe Beacon::States::Ping do
   describe '#call' do
-    describe 'server unreachable and no Internet connection' do
+    context 'server unreachable and no Internet connection' do
       it 'is Offline' do
+        stub_request(:put, 'http://0.0.0.0/api/v1/satellites/')
+          .to_raise(SocketError)
+        stub_request(:head, 'http://example.com/')
+          .to_raise(SocketError)
+
         next_state = Beacon::States::Ping.new.call
+
         expect(next_state).to eq Beacon::States::Offline
       end
     end
 
-    #describe 'server unreachable but online' do
-    #  it 'is Failure' do
-    #    next_state = Beacon::States::Ping.new.call
-    #    assert_equal Beacon::States::Failure, next_state
-    #  end
-    #end
+    context 'server unreachable but online' do
+      it 'is Failure' do
+        stub_request(:put, 'http://0.0.0.0/api/v1/satellites/')
+          .to_raise(SocketError)
+        stub_request(:head, 'http://example.com/')
+          .to_return(status: 200)
+
+        next_state = Beacon::States::Ping.new.call
+        expect(next_state).to eq Beacon::States::Failure
+      end
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,12 @@
 require 'pathname'
-require 'webmock'
-
-root_path = Pathname.new(__FILE__).join('../..').expand_path
-#
-#$LOAD_PATH << root_path.join('lib')
+require 'webmock/rspec'
 require 'beacon'
 
+root_path = Pathname.new(__FILE__).join('../..').expand_path
 test_logger = Logger.new root_path.join('log', 'test.log')
 test_logger.formatter = Beacon.config.send(:default_logger_formatter)
 
 RSpec.configure do |config|
-
   config.disable_monkey_patching!
   config.raise_errors_for_deprecations!
   config.shared_context_metadata_behavior = :apply_to_host_groups
@@ -18,11 +14,10 @@ RSpec.configure do |config|
   config.order = :random if ENV['CI']
 
   config.before :each do
-    Beacon.configure do |config|
-      config.delay = 0
-      config.logger = test_logger
-      config.endpoint = 'http://0.0.0.0'
+    Beacon.configure do |beacon_config|
+      beacon_config.delay = 0
+      beacon_config.logger = test_logger
+      beacon_config.endpoint = 'http://0.0.0.0'
     end
   end
-
 end
